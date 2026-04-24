1c81735d651a07be070231525807b1e4f427b154
y0h4n3s <y0h4n3sk4ssu@proton.me>
2026-04-24 04:22:51 -0700

    Add: copy script for tauri bin resource

--- packages/app-tauri/src-tauri/build.rs
+++ packages/app-tauri/src-tauri/build.rs
@@ -1,4 +1,32 @@
+use std::env;
+use std::path::PathBuf;
+use std::fs;
 fn main() {
+    let target = env::var("TARGET").expect("TARGET not set");
+    let profile = env::var("PROFILE").expect("PROFILE not set");
+
+    let pkg_name = "bridge-sidecar".to_string();
+
+    let orig_binary = if target.contains("windows") {
+        format!("{}.exe", pkg_name)
+    } else {
+        pkg_name.clone()
+    };
+
+    let new_binary = if target.contains("windows") {
+        format!("{}-{}.exe", pkg_name, target)
+    } else {
+        format!("{}-{}", pkg_name, target)
+    };
+    let target_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("..")
+        .join("..")
+        .join("sidecar")
+        .join("target")
+        .join(profile);
+    let source_path = target_dir.join(&orig_binary);
+    let dest_path = target_dir.join(&new_binary);
+    fs::copy(source_path, dest_path).unwrap();
     tauri_build::build()
 }
 
